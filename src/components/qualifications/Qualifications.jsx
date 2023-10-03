468220089cdaf27207ae1b0410481f6d827a3487
Avinash N <73270404+AviN27@users.noreply.github.com>
2023-10-03 16:26:14 +0530

    New changes.

--- src/components/qualifications/Qualifications.jsx
+++ src/components/qualifications/Qualifications.jsx
@@ -17,14 +17,14 @@ const Qualifications = () => {
         <div className="qualification__container container">
             <div className="qualification__tabs">
                 <div className={
-                    toggleState === 1 ? "qualification__button qualification__active button--flex" : "qualification__button button--flex"
+                    toggleState === 1 ? "qualification__button qualification__active button--flex" : "hover-underline-animation qualification__button button--flex"
                     } onClick={() => toggleTab(1)}>
                     <i className="uil uil-graduation-cap qualification__icon"></i>
                      Education
                 </div>
 
                 <div className={
-                    toggleState === 2 ? "qualification__button qualification__active button--flex" : "qualification__button button--flex"
+                    toggleState === 2 ? "qualification__button qualification__active button--flex" : "hover-underline-animation qualification__button button--flex"
                     } onClick={() => toggleTab(2)}>
                     <i className="uil uil-briefcase-alt qualification__icon"></i>
                      Experience
